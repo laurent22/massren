9b2ffcb14c7bc46240995ffd0aeb46e30ebc0fbb
Laurent Cozic <laurent@pogopixels.com>
2014-02-02 17:37:48 +0800

    Test unit for filePathsFromArgs

--- main_test.go
+++ main_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 	"time"
 )
+
+var createdTempFiles []string
 
 func setup(t *testing.T) {
 	pwd, err := os.Getwd()
@@ -20,10 +25,23 @@ func setup(t *testing.T) {
 }
 
 func teardown(t *testing.T) {
-	
+	for _, filePath := range createdTempFiles {
+		os.Remove(filePath)
+	}	
 }
 
-func TestHash(t *testing.T) {
+func createRandomTempFiles() []string {
+	var output []string
+	for i := 0; i < 10; i++ {
+		filePath := fmt.Sprintf("%s/testfile%d", tempFolder(), i)
+		ioutil.WriteFile(filePath, []byte("testing"), 0700)
+		output = append(output, filePath)
+		createdTempFiles = append(createdTempFiles, filePath)
+	}
+	return output
+}
+
+func Test_stringHash(t *testing.T) {
 	if len(stringHash("aaaa")) != 32 {
 		t.Error("hash should be 32 characters long")
 	}
@@ -33,7 +51,7 @@ func TestHash(t *testing.T) {
 	}
 }
 
-func TestConfigFolder(t *testing.T) {
+func Test_configFolder(t *testing.T) {
 	setup(t)
 	defer teardown(t)
 	
@@ -47,7 +65,7 @@ func TestConfigFolder(t *testing.T) {
 	}
 }
 
-func TestWatchFile(t *testing.T) {
+func Test_watchFile(t *testing.T) {
 	setup(t)
 	defer teardown(t)
 	
@@ -69,4 +87,70 @@ func TestWatchFile(t *testing.T) {
 	ioutil.WriteFile(filePath, []byte("testing change"), 0700)	
 
 	<-doneChan
+}
+
+func fileListsAreEqual(files1 []string, files2 []string) error {
+	if len(files1) != len(files2) {
+		return errors.New("file count is different")
+	}
+	
+	for _, f1 := range files1 {
+		found := false
+		for _, f2 := range files2 {
+			if filepath.Base(f1) == filepath.Base(f2) {
+				found = true
+			}
+		}
+		if !found {
+			return errors.New("file names do not match")
+		}
+	}
+
+	return nil
+}
+
+func Test_filePathsFromArgs(t *testing.T) {
+	setup(t)
+	defer teardown(t)
+	
+	tempFiles := createRandomTempFiles()
+	args := []string{
+		tempFolder() + "/*",
+	}
+	
+	filePaths, err := filePathsFromArgs(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	err = fileListsAreEqual(filePaths, tempFiles)
+	if err != nil {
+		t.Error(err)
+	}
+	
+	// If no argument is provided, the function should default to "*"
+	// in the current dir.
+	
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	
+	err = os.Chdir(tempFolder())
+	if err != nil {
+		panic(err)
+	}
+	
+	args = []string{}
+	filePaths, err = filePathsFromArgs(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	err = fileListsAreEqual(filePaths, tempFiles)
+	if err != nil {
+		t.Error(err)
+	}
+
+	os.Chdir(currentDir)
 }
