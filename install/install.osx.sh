8653c550e9e7d086c7aa8f961eb3c27264c77ccb
Laurent Cozic <laurent@pogopixels.com>
2014-02-06 22:13:57 +0800

    Fixed OSX installer

--- install/install.osx.sh
+++ install/install.osx.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-#wget "https://github.com/laurent22/massren/releases/download/v1.0.1/massren.osx.tar.gz"
+wget "https://github.com/laurent22/massren/releases/download/v1.0.1/massren.osx.tar.gz"
 tar xvzf massren.osx.tar.gz
+chmod 755 massren
 mv massren /usr/bin
