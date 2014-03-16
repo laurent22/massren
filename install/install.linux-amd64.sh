8c0d8f71030943395795a4c1103d303a8834c5b3
Laurent Cozic <laurent@pogopixels.com>
2014-03-16 20:00:30 -0300

    Update Linux installer for v1.2.0

--- install/install.linux-amd64.sh
+++ install/install.linux-amd64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ -f "massren.linux-amd64.tar.gz" ]; then mv -f "massren.linux-amd64.tar.gz" "massren.linux-amd64.tar.gz.old" ; fi
-wget "https://github.com/laurent22/massren/releases/download/v1.1.0/massren.linux-amd64.tar.gz"
+wget "https://github.com/laurent22/massren/releases/download/v1.2.0/massren.linux-amd64.tar.gz"
 tar xvzf massren.linux-amd64.tar.gz
 chmod 755 massren
 mv massren /usr/bin
