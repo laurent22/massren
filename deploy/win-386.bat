a06b03ccda4117569eea05265f40304e30c0ef75
Laurent Cozic <laurent@pogopixels.com>
2014-04-29 00:26:30 -0300

    Fixed Win deployment script

--- deploy/win-386.bat
+++ deploy/win-386.bat
@@ -1,4 +1,4 @@
 go build
-/e/Programmes/7-Zip/7z.exe a -tzip -mx=9 massren.win-386.zip massren.exe
+/c/7-Zip/7z.exe a -tzip -mx=9 massren.win-386.zip massren.exe
 mkdir deploy/releases
 mv massren.win-386.zip deploy/releases
