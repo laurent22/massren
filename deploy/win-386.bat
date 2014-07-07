29a881b3d2408462623a0e647f9c720057b590ed
Laurent Cozic <laurent@pogopixels.com>
2014-07-06 23:36:15 -0300

    Improved Win deploy script

--- deploy/win-386.bat
+++ deploy/win-386.bat
@@ -1,4 +1,5 @@
 go build
+rm -f massren.win-386.zip
 /c/7-Zip/7z.exe a -tzip -mx=9 massren.win-386.zip massren.exe
-mkdir deploy/releases
+mkdir -p deploy/releases
 mv massren.win-386.zip deploy/releases
